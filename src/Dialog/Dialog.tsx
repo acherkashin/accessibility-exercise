a90e2c3b531748ae05328e798db82ac0249f2574
Cherkashin Alexander <cherkalexander@gmail.com>
2021-09-17 08:47:09 +0300

    title describes dialog

--- src/Dialog/Dialog.tsx
+++ src/Dialog/Dialog.tsx
@@ -1,6 +1,8 @@
 import FocusTrap from 'focus-trap-react';
 import classNames from 'classnames';
 import './Dialog.css';
+import { useMemo } from 'react';
+import { uuidv4 } from '../utils/guidUtils';
 
 export interface DialogProps {
   title: string;
@@ -13,15 +15,17 @@ export interface DialogProps {
 }
 
 export function Dialog({ title, className, open, children, width, height, onClose }: DialogProps) {
+  const titleId = useMemo(() => uuidv4(), []);
+
   if (!open) {
     return null;
   }
 
   return <FocusTrap>
     <div className="dialog-overlay">
-      <div className={classNames("dialog", className)} role="dialog" style={{ width, height }}>
+      <div aria-labelledby={titleId} className={classNames("dialog", className)} role="dialog" style={{ width, height }}>
         <header className="dialog__header">
-          <h2 className="dialog__title">{title}</h2>
+          <h2 className="dialog__title" id={titleId}>{title}</h2>
           <button onClick={onClose}>Close</button>
         </header>
         <div className="dialog__content">
