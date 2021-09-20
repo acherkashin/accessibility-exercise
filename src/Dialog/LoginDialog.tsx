e04719ae3721cd10e4a8dd39bd6f8a5ded1a00b5
Cherkashin Alexander <cherkalexander@gmail.com>
2021-09-20 08:52:48 +0300

    Make LoginDialog inputs to be controlled

--- src/Dialog/LoginDialog.tsx
+++ src/Dialog/LoginDialog.tsx
@@ -1,3 +1,4 @@
+import { ChangeEventHandler, useCallback, useState } from 'react';
 import { Dialog } from './Dialog';
 import './LoginDialog.css';
 
@@ -7,13 +8,23 @@ export interface LoginDialogProps {
 }
 
 export function LoginDialog({ open, onClose }: LoginDialogProps) {
+  const [login, setLogin] = useState('');
+  const handleLoginChanged: ChangeEventHandler<HTMLInputElement> = useCallback((e) => {
+    setLogin(e.target.value);
+  }, []);
+
+  const [password, setPassword] = useState('');
+  const handlePasswordChanged: ChangeEventHandler<HTMLInputElement> = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+
   return <Dialog className="login-dialog" open={open} title="Вход в личный кабинет" width={700} height={550} onClose={onClose}>
     <div className="login-dialog__content">
       <label className="login-dialog__label">Логин</label>
-      <input className="login-dialog__input" type="text"></input>
+      <input className="login-dialog__input" type="text" onChange={handleLoginChanged} value={login}></input>
 
       <label className="login-dialog__label">Пароль</label>
-      <input className="login-dialog__input" type="text"></input>
+      <input className="login-dialog__input" type="text" onChange={handlePasswordChanged} value={password}></input>
 
       <button className="login-dialog__login">Войти</button>
     </div>
