14b910411a9a60929a1d578a0947d4c8c167f563
Cherkashin Alexander <cherkalexander@gmail.com>
2021-09-28 08:59:34 +0300

    Add Next/Previous buttons

--- src/Carousel/Carousel.tsx
+++ src/Carousel/Carousel.tsx
@@ -1,4 +1,6 @@
 import './Carousel.css';
+import chevronLeft from './assets/chevron-left.png';
+import chevronRight from './assets/chevron-right.png';
 
 export interface Slide {
   id: string;
@@ -10,10 +12,27 @@ export interface CarouselProps {
   slides: Slide[];
 }
 
+/**
+ * https://www.w3.org/WAI/tutorials/carousels/
+ */
 export function Carousel({ slides }: CarouselProps) {
   return (
-    <ul className="carousel">
-      {slides.map(({ id, imageUrl, altText }) => <li key={id}><img src={imageUrl} alt={altText} /></li>)}
-    </ul>
+    <div className="carousel">
+      <ul className="carousel__slides">
+        {slides.map(({ id, imageUrl, altText }) => <li key={id}><img src={imageUrl} alt={altText} /></li>)}
+      </ul>
+      <ul className="carousel__controls">
+        <li>
+          <button className="carousel__button carousel__button--prev">
+            <img src={chevronLeft} alt="Previous Item" />
+          </button>
+        </li>
+        <li>
+          <button className="carousel__button carousel__button--next">
+            <img src={chevronRight} alt="Next Item" />
+          </button>
+        </li>
+      </ul>
+    </div>
   );
 }
