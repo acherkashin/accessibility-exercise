33f26a6d42e81057356a91b03daa09fe091b96f5
Cherkashin Alexander <cherkalexander@gmail.com>
2021-10-01 08:56:38 +0300

    Move buttons above sliders

--- src/Carousel/Carousel.tsx
+++ src/Carousel/Carousel.tsx
@@ -15,13 +15,14 @@ export interface CarouselProps {
 }
 
 /**
- * https://www.w3.org/WAI/tutorials/carousels/
- * TODO: animation
- * TODO: slide should be focused after prev/next buttons
+ * ✅ https://www.w3.org/WAI/tutorials/carousels/
+ * Прочитать - https://www.w3.org/TR/wai-aria-practices-1.1/examples/carousel/carousel-1.html
+ * [Доступная карусель - Юлия Долгун](https://www.youtube.com/watch?v=DAO9_bXCTuk)
+ * TODO: animation. scroll snap?
  */
 export function Carousel({ slides }: CarouselProps) {
-  const [current, setCurrent] = useState(slides[0].id);
-  const currentIndex = slides.findIndex(item => item.id === current);
+  const [currentId, setCurrent] = useState(slides[0].id);
+  const currentIndex = slides.findIndex(item => item.id === currentId);
 
   const selectNext = () => {
     let nextIndex = currentIndex + 1;
@@ -42,28 +43,28 @@ export function Carousel({ slides }: CarouselProps) {
   };
 
   return (
-    <div className="carousel">
+    <div className="carousel" aria-roledescription="Карусель">
+      <ul className="carousel__controls">
+        <li>
+          <button className="carousel__button carousel__button--prev" onClick={selectPrev} aria-label="Предыдущий слайд">
+            <img src={chevronLeft} role="presentation" alt="" />
+          </button>
+        </li>
+        <li>
+          <button className="carousel__button carousel__button--next" onClick={selectNext} aria-label="Следующий слайд">
+            <img src={chevronRight} role="presentation" alt="" />
+          </button>
+        </li>
+      </ul>
       <ul className="carousel__slides">
         {slides.map(({ id, imageUrl, altText }) =>
-          <li key={id} className={classNames("carousel__slide-item", {
-            "carousel__slide-item--current": id === current,
+          <li key={id} tabIndex={id === currentId ? 0 : undefined} className={classNames("carousel__slide-item", {
+            "carousel__slide-item--current": id === currentId,
           })}>
             <img className="carousel__slide-item-content" src={imageUrl} alt={altText} />
           </li>)}
       </ul>
-      <ul className="carousel__controls">
-        <li>
-          <button className="carousel__button carousel__button--prev" onClick={selectPrev}>
-            <img src={chevronLeft} alt="Предыдущий слайд" />
-          </button>
-        </li>
-        <li>
-          <button className="carousel__button carousel__button--next" onClick={selectNext}>
-            <img src={chevronRight} alt="Следующий слайд" />
-          </button>
-        </li>
-      </ul>
-      <div aria-live="polite" aria-atomic="true" className="liveregion visually-hidden">{slides[currentIndex].altText} Слайд {currentIndex + 1} из {slides.length}</div>
+      <div aria-live="polite" aria-atomic="true" className="liveregion visually-hidden">Слайд {currentIndex + 1} из {slides.length}</div>
     </div>
   );
 }
