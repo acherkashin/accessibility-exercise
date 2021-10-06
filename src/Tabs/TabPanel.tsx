2a62d2ed0f524983b19df9cf89fe3087eca5794b
Cherkashin Alexander <cherkalexander@gmail.com>
2021-10-06 08:50:05 +0300

    Apply styles for tabs

--- src/Tabs/TabPanel.tsx
+++ src/Tabs/TabPanel.tsx
@@ -9,7 +9,7 @@ export interface TabPanelProps {
 
 export function TabPanel({ children, index, selected }: TabPanelProps) {
   return <div id={`tabpanel_${index}`}
-    className={classNames('tab', { 'tab--active': selected })}
+    className={classNames('tabs__panel', { 'tabs__panel--active': selected })}
     hidden={!selected}
   >
     {children}
--- src/Tabs/Tabs.tsx
+++ src/Tabs/Tabs.tsx
@@ -1,5 +1,8 @@
+import classNames from "classnames";
 import React, { useState } from "react";
 import { TabPanelProps } from "./TabPanel";
+
+import './Tabs.css'
 
 export interface TabsProps {
   // TODO: replace with correct type. We should be able to pass sinle element to tabs. look how it is impelemented in material-ui.
@@ -15,8 +18,10 @@ export function Tabs({ children }: TabsProps) {
   const [selectedTab, selectTab] = useState(tabs.find(tab => tab.selected)?.title ?? tabs[0].title);
 
   return <div className="tabs">
-    <div role="tablist">
-      {tabs.map(item => <button role="tab" onClick={() => selectTab(item.title)}>{item.title}</button>)}
+    <div className="tabs__tab-list" role="tablist">
+      {tabs.map(item => <button className={classNames("tabs__tab", {
+        "tabs__tab--active": selectedTab === item.title
+      })} role="tab" onClick={() => selectTab(item.title)}>{item.title}</button>)}
     </div>
     <div>
       {React.Children.map(children, (item, i) => React.cloneElement(item, {
@@ -24,5 +29,5 @@ export function Tabs({ children }: TabsProps) {
         selected: selectedTab === (item.props as TabPanelProps).title
       }))}
     </div>
-  </div>
+  </div >
 }
